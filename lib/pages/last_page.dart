3847a0fa07a440f394863638ff215b1a8e7bcd32
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-06 17:14:54 +0500

    Initial commit

---
+++ lib/pages/last_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LastPage extends StatefulWidget {
+  const LastPage({super.key});
+
+  @override
+  State<LastPage> createState() => _LastPageState();
+}
+
+class _LastPageState extends State<LastPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/second_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}

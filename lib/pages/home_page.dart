bf8c7a1cafff8877a901ac3727863d8d415a6dce
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-06 18:42:20 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,62 +11,50 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   GlobalKey key1 = GlobalKey();
+  GlobalKey key2 = GlobalKey();
+  GlobalKey key3 = GlobalKey();
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((value) {
-      ShowCaseWidget.of(context).startShowCase([key1]);
+      ShowCaseWidget.of(context).startShowCase([key1, key2, key3]);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
           child: Column(
             children: [
-              Spacer(),
-              Showcase(
-                key: key1,
-                title: "Order Now button",
-                titleTextStyle: TextStyle(
-                  color: Colors.deepPurple,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w700,
-                ),
-                description: 'Press this button to order',
-                descTextStyle: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                ),
-                descriptionTextAlign: TextAlign.center,
-                showArrow: true,
-                tooltipActions: [
-                  TooltipActionButton(
-                    type: TooltipDefaultActionType.previous,
-                    backgroundColor: Colors.red,
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                    ),
-                    leadIcon: ActionButtonIcon(icon: )
-                  ),
-                  TooltipActionButton(
-                    type: TooltipDefaultActionType.next,
-                    backgroundColor: Colors.blue,
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
+              Row(
+                children: [
+                  _IntroWidget(
+                    key: key2,
+                    title: "Your cart",
+                    desc: "Her are all your carts",
+                    child: Text(
+                      "Your cart",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
                   ),
                 ],
+              ),
+              Spacer(),
+              _IntroWidget(
+                key: key1,
+                title: "Order Now button",
+                desc: "To order press this Button",
                 child: CupertinoButton(
-                  onPressed: () {},
                   color: Colors.yellow,
+                  onPressed: () {},
                   child: Center(
                     child: Text("Order Now"),
                   ),
@@ -76,6 +64,75 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _IntroWidget extends StatelessWidget {
+  final GlobalKey key;
+  final String title;
+  final String desc;
+  final Widget child;
+
+  const _IntroWidget({
+    required this.key,
+    required this.title,
+    required this.desc,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Showcase(
+      blurValue: 5,
+      key: key,
+      title: title,
+      titleTextStyle: TextStyle(
+        color: Colors.deepPurple,
+        fontSize: 15,
+        fontWeight: FontWeight.w700,
+      ),
+      description: desc,
+      descTextStyle: TextStyle(
+        color: Colors.blue,
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+      ),
+      descriptionTextAlign: TextAlign.center,
+      showArrow: true,
+      tooltipActions: [
+        TooltipActionButton(
+          type: TooltipDefaultActionType.previous,
+          backgroundColor: Colors.red,
+          textStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+          leadIcon: ActionButtonIcon(
+            icon: Icon(
+              CupertinoIcons.chevron_left,
+              size: 20,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        TooltipActionButton(
+          type: TooltipDefaultActionType.next,
+          backgroundColor: Colors.blue,
+          textStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+          tailIcon: ActionButtonIcon(
+            icon: Icon(
+              CupertinoIcons.chevron_right,
+              size: 20,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+      child: child,
     );
   }
 }
